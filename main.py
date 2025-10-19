72007c4592d4f320a873a5ffe2d3f5e5a9939785
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 16:18:08 +0100

    oop

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ import os
 
 load_dotenv()
 
-DATABASE = DataBaseContoller()
-INVOICE_MANAGER = PayPalInvoiceController(os.getenv("PAYPAL_API_TOKEN"), os.getenv("VENDOR_NAME"))
+class App:
+    def __init__(self):
+        self.database = DataBaseContoller()
+        self.invoice_manager = PayPalInvoiceController(os.getenv("PAYPAL_API_TOKEN"), os.getenv("VENDOR_NAME"))
 
-def create_local_invoice():
-    last_tag = DATABASE.get_last_created_invoice()
-    if last_tag is None:
-        new_tag = 1
-    else:
-        new_tag = last_tag[0] + 1
-    DATABASE.insert_new_invoice(new_tag)
-    return new_tag
+    def create_local_invoice(self):
+        last_tag = self.database.get_last_created_invoice()
+        if last_tag is None:
+            new_tag = 1
+        else:
+            new_tag = last_tag[0] + 1
+        self.database.insert_new_invoice(new_tag)
+        return new_tag
+
+    def create_draft_invoice(self, invoice_tag):
+        self.invoice_manager.create_invoice(invoice_tag)
 
 def main():
-    create_local_invoice()
+    app = App()
+    invoice_tag = app.create_local_invoice()
+    app.create_draft_invoice(invoice_tag)
 
 if __name__ == "__main__":
     main()
