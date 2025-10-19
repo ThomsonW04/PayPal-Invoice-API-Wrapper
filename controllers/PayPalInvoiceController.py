11c3e663486beabb41b10e89a7bc8ecceeac05a8
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 16:27:06 +0100

    more info about vendr

--- controllers/PayPalInvoiceController.py
+++ controllers/PayPalInvoiceController.py
@@ -1,19 +1,25 @@
 import requests
 
 class PayPalInvoiceController:
-    def __init__(self, api_token, vendor, invoice_prefix, currency_code):
+    def __init__(self, api_token, vendor_given_names, vendor_last_names, vendor_email, invoice_prefix, currency_code):
         self.invoice_api_url = "https://api-m.sandbox.paypal.com/v2/invoicing/invoices"
         self.headers = {
             'Authorization': f'Bearer {api_token}',
             'Content-Type': 'application/json',
             'Prefer': 'return=representation',
         }
-        self.vendor = vendor
+        self.vendor_given_names = vendor_given_names
+        self.vendor_last_names = vendor_last_names
+        self.vendor_email = vendor_email
         self.invoice_prefix = invoice_prefix
         self.currency_code = currency_code
 
-    def create_invoice(self, invoice_id):
+    def create_invoice(self, invoice_id, note):
         body = {
-
+            "detail": {
+                "invoice_number": self.invoice_prefix + invoice_id,
+                "currency_code": self.currency_code,
+                "note": note
+            }
         }
         
--- main.py
+++ main.py
@@ -8,7 +8,12 @@ load_dotenv()
 class App:
     def __init__(self):
         self.database = DataBaseContoller()
-        self.invoice_manager = PayPalInvoiceController(os.getenv("PAYPAL_API_TOKEN"), os.getenv("VENDOR_NAME"), os.getenv("INVOICE_PREFIX"), os.getenv("CURRENCY_CODE"))
+        self.invoice_manager = PayPalInvoiceController(api_token = os.getenv("PAYPAL_API_TOKEN"), 
+                                                       vendor_given_names = os.getenv("VENDOR_GIVEN_NAMES"), 
+                                                       vendor_last_names = os.getenv("VENDOR_LAST_NAMES"), 
+                                                       vendor_email = os.getenv("VENDOR_EMAIL"), 
+                                                       invoice_prefix = os.getenv("INVOICE_PREFIX"), 
+                                                       currency_code = os.getenv("CURRENCY_CODE"))
 
     def create_local_invoice(self):
         last_tag = self.database.get_last_created_invoice()
@@ -19,13 +24,13 @@ class App:
         self.database.insert_new_invoice(new_tag)
         return new_tag
 
-    def create_draft_invoice(self, invoice_tag):
-        self.invoice_manager.create_invoice(invoice_tag)
+    def create_draft_invoice(self, invoice_tag, note):
+        self.invoice_manager.create_invoice(invoice_tag, note)
 
 def main():
     app = App()
     invoice_tag = app.create_local_invoice()
-    app.create_draft_invoice(invoice_tag)
+    app.create_draft_invoice(invoice_tag, "Pleasure doing business")
 
 if __name__ == "__main__":
     main()
