e563ff0792b165d5361bbb9255e3605bf1ba5464
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 16:21:13 +0100

    cur code

--- controllers/PayPalInvoiceController.py
+++ controllers/PayPalInvoiceController.py
@@ -1,8 +1,7 @@
 import requests
 
 class PayPalInvoiceController:
-    def __init__(self, api_token, vendor, invoice_prefix):
-        self.invoice_prefix = invoice_prefix
+    def __init__(self, api_token, vendor, invoice_prefix, currency_code):
         self.invoice_api_url = "https://api-m.sandbox.paypal.com/v2/invoicing/invoices"
         self.headers = {
             'Authorization': f'Bearer {api_token}',
@@ -10,6 +9,8 @@ class PayPalInvoiceController:
             'Prefer': 'return=representation',
         }
         self.vendor = vendor
+        self.invoice_prefix = invoice_prefix
+        self.currency_code = currency_code
 
     def create_invoice(self, invoice_id):
         body = {
